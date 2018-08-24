2005bf51327393d9f54ea59500be57cecc22d4df
Photon Cat <lkstc112233@gmail.com>
2018-08-24 15:14:11 -0400

    Corrected misspelling.

--- src/java/com/photoncat/digitalpet/Statuses.java
+++ src/java/com/photoncat/digitalpet/Statuses.java
@@ -4,10 +4,10 @@ package com.photoncat.digitalpet;
  * A class holding everything status of a digital pet.
  */
 public class Statuses {
-	public Statuses(long timestamp, int hunger, int cleaness) {
+	public Statuses(long timestamp, int hunger, int cleanness) {
 		this.timestamp = timestamp;
 		this.hunger = hunger;
-		this.cleaness = cleaness;
+		this.cleanness = cleanness;
 	}
 	
 	// These fields will be modified to private with some getters in 
@@ -15,5 +15,5 @@ public class Statuses {
 	// They are as they like because this is simple.
 	public long timestamp;
 	public int hunger;
-	public int cleaness;
+	public int cleanness;
 }
