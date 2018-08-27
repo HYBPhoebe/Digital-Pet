1b6dccabcd5c15cda79757467fca63a9ac90e341
houyanbing <hou_yanbing@gwu.edu>
2018-08-27 14:52:27 -0400

    implemented partly donothing

--- src/java/com/photoncat/digitalpet/control/Pet.java
+++ src/java/com/photoncat/digitalpet/control/Pet.java
@@ -4,18 +4,44 @@ import com.photoncat.digitalpet.Statuses;
 
 public class Pet implements com.photoncat.digitalpet.Pet{
 
-    @Override
-    public Statuses doNothing(long timestamp) {
-        return null;
+
+    private int hunger=0;
+    private long timestamp;
+    private int cleanness;
+    private int pro=0;//last hunger value;
+    private long protime;//last post time;
+    private int pc=0;//last cleanness value;
+     private Statuses pet= new com.photoncat.digitalpet.Statuses(timestamp, hunger, cleanness);
+
+
+    public Statuses doNothing(long timestamp){
+        if(pro==0){
+            protime=timestamp;
+        }
+        else {
+            hunger = (int) (pro + (timestamp - protime) / 10);
+            pro = hunger;
+            // cleanness=(int)(pc+(timestamp-protime)/5);
+            //pc=cleanness;
+            protime = timestamp;
+        }
+       return new Statuses (timestamp, hunger,0);
     }
 
     @Override
     public Statuses feed(long timestamp) {
-        return null;
+
+
+
+       return null;
+
+
     }
 
     @Override
     public Statuses bath(long timestamp) {
-        return null;
+       return null;
+
+
     }
 }
