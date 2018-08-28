1a2912800ad979ffaae36526a36b98cf6ebaf3cb
houyanbing <hou_yanbing@gwu.edu>
2018-08-28 08:23:26 -0400

    implemented cleanness

--- src/java/com/photoncat/digitalpet/control/Pet.java
+++ src/java/com/photoncat/digitalpet/control/Pet.java
@@ -24,17 +24,27 @@ public class Pet implements com.photoncat.digitalpet.Pet{
         }
         else {
 
-            hunger = (int) (pro + (timestamp - protime) / 10);
-            // if (hunger > 100) {
-            //   hunger = 100;
-            //}
-            pro = hunger;
+            hunger = (int) (pro + (timestamp - protime) / 0.5);
 
-            if (pro > 100) {
+            if (hunger > 100) {
                 hunger=100;
                 pro = hunger;
 
             } else {
+                pro = hunger;
+            }
+
+
+            cleanness=(int)(pc-((timestamp - protime))/10);
+            if(cleanness<0){
+                cleanness=0;
+                pc=cleanness;
+            }
+            else{
+                pc=cleanness;
+            }
+
+
                 if ((timestamp - protime) < 10) {
                     n -= (int) (timestamp - protime);
                 } else {
@@ -43,37 +53,37 @@ public class Pet implements com.photoncat.digitalpet.Pet{
                 }
             }
 
-        }
+
+
+
+
             return new Statuses(timestamp, hunger, cleanness);
 
     }
-        /**cleanness = (int) (pc - (timestamp - protime) / 10);
-         if(cleanness>=0) {
-         pc = cleanness;
-         }
-         else{
-         pc=0;
-         }**/
 
         @Override
         public Statuses feed ( long timestamp){
 
             hunger = pro - 20;
-            if (hunger < 0) {
+            while (hunger < 0) {
                 hunger = 0;
             }
             pro = hunger;
 
-            return new Statuses(timestamp, hunger, 0);
+            return new Statuses(timestamp, hunger,cleanness);
 
         }
 
     @Override
     public Statuses bath(long timestamp) {
 
+     cleanness=pc+20;
+     while(cleanness>100){
+         cleanness=100;
+     }
+     pc=cleanness;
 
 
-       return null;
-
+     return new Statuses(timestamp,hunger,cleanness);
     }
 }
