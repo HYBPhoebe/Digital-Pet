9aa3f627893341310c46cf5b4d0f1b3695d7fc83
Photon Cat <lkstc112233@gmail.com>
2018-08-29 15:30:17 -0400

    Applied builder to pets

--- src/java/com/photoncat/digitalpet/ui/DummyPet.java
+++ src/java/com/photoncat/digitalpet/ui/DummyPet.java
@@ -6,16 +6,16 @@ import com.photoncat.digitalpet.Statuses;
 public class DummyPet implements Pet {
     @Override
     public Statuses doNothing(long timestamp) {
-        return new Statuses(timestamp, 0, 0);
+        return null;
     }
 
     @Override
     public Statuses feed(long timestamp) {
-        return new Statuses(timestamp, 0, 0);
+        return null;
     }
 
     @Override
     public Statuses bath(long timestamp) {
-        return new Statuses(timestamp, 0, 0);
+        return null;
     }
 }
