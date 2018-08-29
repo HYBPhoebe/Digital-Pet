beaf3d55648e7af0d79918e573b803ce7b6209f6
houyanbing <hou_yanbing@gwu.edu>
2018-08-29 14:33:49 -0400

    changed format

--- src/java/com/photoncat/digitalpet/control/Pet.java
+++ src/java/com/photoncat/digitalpet/control/Pet.java
@@ -2,88 +2,75 @@ package com.photoncat.digitalpet.control;
 
 import com.photoncat.digitalpet.Statuses;
 
-public class Pet implements com.photoncat.digitalpet.Pet{
+public class Pet implements com.photoncat.digitalpet.Pet {
 
 
-    private int hunger=0;
+    private int hunger = 0;
     private long timestamp;
     private int cleanness;
-    private int pro=0;//last hunger value;
+    private int pro = 0;//last hunger value;
 
     private long protime;//last post time;
-
-    private int pc=100;//last cleanness value;
-    private int n=0;//time difference;
-    private int c=0;// cleaness difference;
-    private Statuses pet= new com.photoncat.digitalpet.Statuses(timestamp, hunger, cleanness);
+    private int pc = 100;//last cleanness value;
+    private int n = 0;//time difference;
+    private int c = 0;// cleaness difference;
+    private Statuses pet = new com.photoncat.digitalpet.Statuses(timestamp, hunger, cleanness);
 
 
     public Statuses doNothing(long timestamp) {
         if (protime == 0) {
             protime = timestamp;
-        }
-        else {
+        } else {
 
-            hunger = (int) (pro + (timestamp - protime) / 0.5);
+            hunger = (int) (pro + (timestamp - protime) / 10);
 
             if (hunger > 100) {
-                hunger=100;
+                hunger = 100;
                 pro = hunger;
-
             } else {
                 pro = hunger;
             }
 
-
-            cleanness=(int)(pc-((timestamp - protime))/10);
-            if(cleanness<0){
-                cleanness=0;
-                pc=cleanness;
-            }
-            else{
-                pc=cleanness;
+            cleanness = (int) (pc - ((timestamp - protime)) / 10);
+            if (cleanness < 0) {
+                cleanness = 0;
+                pc = cleanness;
+            } else {
+                pc = cleanness;
             }
 
-
-                if ((timestamp - protime) < 10) {
-                    n -= (int) (timestamp - protime);
-                } else {
-                    n -= 10;
-                    protime = timestamp;
-                }
+            if ((timestamp - protime) < 10) {
+                n -= (int) (timestamp - protime);
+            } else {
+                n -= 10;
+                protime = timestamp;
             }
+        }
 
-
-
-
-
-            return new Statuses(timestamp, hunger, cleanness);
-
+        return new Statuses(timestamp, hunger, cleanness);
     }
 
-        @Override
-        public Statuses feed ( long timestamp){
+    @Override
+    public Statuses feed(long timestamp) {
 
-            hunger = pro - 20;
-            while (hunger < 0) {
-                hunger = 0;
-            }
-            pro = hunger;
-
-            return new Statuses(timestamp, hunger,cleanness);
-
+        hunger = pro - 20;
+        while (hunger < 0) {
+            hunger = 0;
         }
+        pro = hunger;
+
+        return new Statuses(timestamp, hunger, cleanness);
+    }
 
     @Override
     public Statuses bath(long timestamp) {
 
-     cleanness=pc+20;
-     while(cleanness>100){
-         cleanness=100;
-     }
-     pc=cleanness;
+        cleanness = pc + 20;
+        while (cleanness > 100) {
+            cleanness = 100;
+        }
+        pc = cleanness;
 
-
-     return new Statuses(timestamp,hunger,cleanness);
+        return new Statuses(timestamp, hunger, cleanness);
     }
 }
