cae1cf7149038224709d12253bb5b8f762b7ae3a
Photon Cat <lkstc112233@gmail.com>
2018-08-30 15:33:17 -0400

    Implemented a simple animation in dummy pet

--- src/java/com/photoncat/digitalpet/ui/DummyPet.java
+++ src/java/com/photoncat/digitalpet/ui/DummyPet.java
@@ -4,9 +4,14 @@ import com.photoncat.digitalpet.Pet;
 import com.photoncat.digitalpet.Statuses;
 
 public class DummyPet implements Pet {
+    double x = 0;
+    double y = 0;
+
     @Override
     public Statuses doNothing(long timestamp) {
-        return null;
+        x = 100 + 50 * Math.cos(timestamp / 5.);
+        y = 100 + 50 * Math.sin(timestamp / 8.);
+        return new Statuses.Builder().setX(x).setY(y).build();
     }
 
     @Override
